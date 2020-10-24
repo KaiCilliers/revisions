344265a71c807133a51aaf26731f590ba091d70b
Kai Cilliers <kaijohan428@gmail.com>
2020-10-24 23:43:27 +0100

    gooihom

--- app/src/main/java/com/example/testzone/trivia/TriviaActivity.kt
+++ app/src/main/java/com/example/testzone/trivia/TriviaActivity.kt
@@ -3,25 +3,29 @@ package com.example.testzone.trivia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.testzone.R
 import com.example.testzone.databinding.ActivityTriviaBinding
 
 class TriviaActivity : AppCompatActivity() {
+    private val binding: ActivityTriviaBinding by lazy {
+        DataBindingUtil.setContentView<ActivityTriviaBinding>(this, R.layout.activity_trivia)
+    }
+    private val drawerLayout: DrawerLayout by lazy { binding.drawerLayout }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        @Suppress("UNUSED_VARIABLE")
-        val binding =
-            DataBindingUtil.setContentView<ActivityTriviaBinding>(this, R.layout.activity_trivia)
-        NavigationUI.setupActionBarWithNavController(
-            this, findNavController(R.id.navHostFragment)
-        )
         NavigationUI.setupWithNavController(
             binding.navDrawerView,
             findNavController(R.id.navHostFragment)
         )
+        NavigationUI.setupActionBarWithNavController(
+            this, findNavController(R.id.navHostFragment), drawerLayout
+        )
     }
 
-    override fun onSupportNavigateUp(): Boolean = findNavController(R.id.navHostFragment).navigateUp()
+    override fun onSupportNavigateUp(): Boolean = NavigationUI.navigateUp(
+        findNavController(R.id.navHostFragment), drawerLayout
+    )
 }
