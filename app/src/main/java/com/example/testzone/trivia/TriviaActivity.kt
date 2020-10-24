bc91884e6050eb7f2728d8fcf03b5347881ba847
Kai Cilliers <kaijohan428@gmail.com>
2020-10-24 20:14:20 +0100

    gooihom

---
+++ app/src/main/java/com/example/testzone/trivia/TriviaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testzone.trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testzone.R
+
+class TriviaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trivia)
+    }
+}
