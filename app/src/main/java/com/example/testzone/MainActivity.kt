75abddcdc9967ef8df8dee93d9d4af134a9126ff
Kai Cilliers <kaijohan428@gmail.com>
2020-10-24 13:01:38 +0100

    gooihom

--- app/src/main/java/com/example/testzone/MainActivity.kt
+++ app/src/main/java/com/example/testzone/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,25 +13,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_roll.clickAction { toast("Button clicked"); rollDice() }
-        btn_count_up.clickAction { incrementLabel(tv_result) }
-        btn_reset.clickAction { clear(tv_result) }
+        btn_roll.clickAction { rollDice(img_dice, img_dice_two) }
+        btn_count_up.gone()
+        btn_reset.gone()
     }
-    private fun rollDice() {
-        tv_result.text = (1..6).random().toString()
+    private fun rollDice(vararg views: ImageView) {
+        for(view in views) {
+            view.setImageResource(when(randomValue()) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.empty_dice
+            })
+        }
     }
-    private fun incrementLabel(tv: TextView) {
-       if(tv.text.toString() == getString(R.string.label_hello_world)) {
-           tv.text = "1"
-       } else {
-           when(val intVal = "${tv.text}".toInt()) {
-               6 -> tv.text = "$intVal"
-               else -> tv.text = "${intVal.inc()}"
-           }
-       }
-    }
-    private fun clear(tv: TextView) = tv.clear("0")
+    private fun randomValue() = (1..6).random()
+
 }
 inline fun View.clickAction(crossinline action: () -> Unit) = setOnClickListener{action()}
 fun Context.toast(message: String) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 fun TextView.clear(withText: String = "") { this.text = withText }
+fun View.gone() { visibility = View.GONE }
