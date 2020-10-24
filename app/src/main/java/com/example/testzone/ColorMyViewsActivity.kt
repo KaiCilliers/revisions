05c907b9d64283f75549c7d2255d9c0b377e5257
Kai Cilliers <kaijohan428@gmail.com>
2020-10-24 17:06:17 +0100

    gooihom

---
+++ app/src/main/java/com/example/testzone/ColorMyViewsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testzone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorMyViewsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_my_views)
+    }
+}
