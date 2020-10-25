08db4be1222ac0dc2b6a98d75a5b28d21d760d14
Kai Cilliers <kaijohan428@gmail.com>
2020-10-25 19:24:57 +0000

    gooihom

--- app/src/main/java/com/example/testzone/guess/game/GameViewModel.kt
+++ app/src/main/java/com/example/testzone/guess/game/GameViewModel.kt
@@ -1,2 +1,60 @@
 package com.example.testzone.guess.game
 
+import androidx.lifecycle.ViewModel
+import timber.log.Timber
+
+class GameViewModel : ViewModel() {
+    var word = ""
+    var score = 0
+    private val wordList: MutableList<String> by lazy {
+        mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+    }
+    private fun resetList() {
+        wordList.shuffle()
+    }
+    init {
+        resetList()
+        nextWord()
+        Timber.e("GameViewModel created!")
+    }
+
+    private fun nextWord() {
+        if(!wordList.isEmpty())
+            word = wordList.removeAt(0)
+    }
+    fun onSkip(){
+        score--
+        nextWord()
+    }
+    fun onCorrect() {
+        score++
+        nextWord()
+    }
+
+    override fun onCleared() {
+        Timber.e("GameViewModel destroyed!")
+        super.onCleared()
+    }
+}
--- app/src/main/java/com/example/testzone/guess/game/GuessGameFragment.kt
+++ app/src/main/java/com/example/testzone/guess/game/GuessGameFragment.kt
@@ -5,15 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.testzone.clickAction
 import com.example.testzone.databinding.GuessGameFragmentBinding
 
 class GuessGameFragment : Fragment() {
+    val viewModel: GameViewModel by lazy {
+        ViewModelProvider(this).get(GameViewModel::class.java)
+    }
+    private lateinit var binding: GuessGameFragmentBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = GuessGameFragmentBinding.inflate(inflater)
+        binding = GuessGameFragmentBinding.inflate(inflater)
+        binding.correctButton.clickAction { onCorrect() }
+        binding.skipButton.clickAction { onSkip() }
+        updateScoreText()
+        updateWordText()
         return binding.root
+    }
+    private fun onSkip() {
+        viewModel.onSkip()
+        updateWordText()
+        updateScoreText()
+    }
+
+    private fun onCorrect() {
+        viewModel.onCorrect()
+        updateWordText()
+        updateScoreText()
+    }
+    private fun updateWordText() {
+        binding.wordText.text = viewModel.word
+    }
+
+    private fun updateScoreText() {
+        binding.scoreText.text = viewModel.score.toString()
     }
 }
