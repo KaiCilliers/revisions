e815c00b09717b7fdea21a964badc123a7486d51
Kai Cilliers <kaijohan428@gmail.com>
2020-10-26 11:09:12 +0000

    gooihom

--- app/src/main/java/com/example/testzone/sleep/database/SleepDao.kt
+++ app/src/main/java/com/example/testzone/sleep/database/SleepDao.kt
@@ -1,2 +1,28 @@
 package com.example.testzone.sleep.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface SleepDao {
+    @Insert
+    fun insert(night: SleepNightEntity)
+
+    @Update
+    fun update(night: SleepNightEntity)
+
+    @Query("SELECT * FROM daily_sleep_quality_table WHERE id = :id")
+    fun night(id: Long): SleepNightEntity?
+
+    @Query("SELECT * FROM daily_sleep_quality_table")
+    fun all(): LiveData<List<SleepNightEntity>>
+
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY id DESC LIMIT 1")
+    fun recent(): SleepNightEntity?
+
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+}
--- app/src/main/java/com/example/testzone/sleep/database/SleepDatabase.kt
+++ app/src/main/java/com/example/testzone/sleep/database/SleepDatabase.kt
@@ -1,2 +1,33 @@
 package com.example.testzone.sleep.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [SleepNightEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class SleepDatabase : RoomDatabase() {
+    abstract val sleepDao: SleepDao
+
+    companion object {
+        @Volatile
+        private lateinit var INSTANCE: SleepDatabase
+        fun instance(context: Context): SleepDatabase {
+            synchronized(this) {
+                if(!::INSTANCE.isInitialized) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    ).fallbackToDestructiveMigration()
+                        .build()
+                }
+                return INSTANCE
+            }
+        }
+    }
+}
