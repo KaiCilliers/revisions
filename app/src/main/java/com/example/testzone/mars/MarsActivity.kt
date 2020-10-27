5eb7f34a8dab53a2c624f3c5f9b058d07aab664f
Kai Cilliers <kaijohan428@gmail.com>
2020-10-27 21:25:59 +0000

    gooihom

---
+++ app/src/main/java/com/example/testzone/mars/MarsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testzone.mars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testzone.R
+
+class MarsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mars)
+    }
+}
