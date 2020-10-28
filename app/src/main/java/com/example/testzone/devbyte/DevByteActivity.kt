66144a4485974ed99d849b4fda08acd70dcfc5a9
Kai Cilliers <kaijohan428@gmail.com>
2020-10-28 12:21:47 +0000

    gooihom

---
+++ app/src/main/java/com/example/testzone/devbyte/DevByteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testzone.devbyte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testzone.R
+
+class DevByteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dev_byte)
+    }
+}
